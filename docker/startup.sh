95e09d508ef20ba16285e743905a16822f900281
Iain Houston <iainhouston@mac.com>
2016-07-24 11:56:15 +0100

    Compatible with Docker for Mac

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # create the environment file for Docker
-env -i environment | sort > .environment.env
+env -i ./environment | sort > ./.environment.env
 
 # load functions and environment variables
 . functions
 
 # start docker containers
-export COMPOSE_OPTIONS="--env-file=.environment.env"
+export COMPOSE_OPTIONS="--env-file=./.environment.env"
 docker-compose --project-name "$PROJECT_NAME" up
 
 # cleanup
